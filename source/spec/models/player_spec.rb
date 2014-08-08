bdf40bcac9dafef9ec5a41d4f86c6159c566da45
Anthony Edwards Jr & Cassie Moy <pair+aedwards12.cassiemoy@devbootcamp.com>
2014-08-08 15:44:34 -0400

    Set up RSpec test environment.

---
+++ source/spec/models/player_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Player do
+  it "is valid with firstname, lastname, position, rank, byeweek, ADP, "
+  it "is invalid without firstname"
+  it "is invalid without lastname"
+  it "is invalid without rank"
+  it "is invalid without position"
+  it "is invalid without rank"
+  it "is invalid without byeweek"
+  it "is invalid without ADP"
+end
