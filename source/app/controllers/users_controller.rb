730809eb6e348f5d1b01bc001d8b129963014d00
Sid Patel <sidpatel13@gmail.com>
2014-08-08 17:50:15 -0400

    added routes for sign up page which now works

--- source/app/controllers/users_controller.rb
+++ source/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def login
+
+  end
+
   def authenticate
   	@user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
@@ -10,9 +14,29 @@ class UsersController < ApplicationController
     end
   end
 
-  def login
+  def signup
 
   end
 
+  def create_user
+    @user = User.new(user_params(params))
+    if @user.save
+      session[:user] = @user.id
+      redirect_to '/', flash: {notice: 'Successful log in!'}
+    else
+      redirect_to signup, flash: {notice: 'Failed'}
+    end
+  end
+
+  def sign_out
+    session.clear
+    redirect_to '/'
+  end
+
+private
+
+  def user_params(params)
+    params.require(:user).permit(:email, :password)
+  end
 
 end
