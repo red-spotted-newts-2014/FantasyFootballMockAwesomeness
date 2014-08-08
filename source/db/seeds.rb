7a36356296469fe7b4dc035bcfe68d303452ad31
Sid Patel <sidpatel13@gmail.com>
2014-08-07 22:46:44 -0400

    test seed file to check models and associations

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+sid = User.create(email: "sidpatel13@gmail.com", password_digest: "password")
+
+draft1 = sid.drafts.create()
+
+round = Round.create(round_number: 1, draft_id: draft1.id)
+
+pick = round.picks.create(pick_number: 1, user_id: 1, round_id: 1)	
+
+Player.create(rank: 1, first_name: "Adrian", last_name: "Peterson", position: "RB", bye_week: 7, ADP: 2.24, pick_id: 1)
