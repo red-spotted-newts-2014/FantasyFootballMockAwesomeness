b420bfe7a95ac6f3a755eeb85c75502023493c42
fahia mohamed <fahia_mohamed@hotmail.com>
2014-08-08 12:27:44 -0400

    changed the seed file for testing- was trying to see if a pick can belong to a draft and not a user

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -10,8 +10,7 @@ sid = User.create(email: "sidpatel13@gmail.com", password_digest: "password")
 
 draft1 = sid.drafts.create()
 
-round = Round.create(round_number: 1, draft_id: draft1.id)
-
-pick = round.picks.create(pick_number: 1, user_id: 1, round_id: 1)	
+pick = draft1.picks.create(pick_number: 1, user_id: 1)	
+pick2 = draft1.picks.create(pick_number: 2)
 
 Player.create(rank: 1, first_name: "Adrian", last_name: "Peterson", position: "RB", bye_week: 7, ADP: 2.24, pick_id: 1)
