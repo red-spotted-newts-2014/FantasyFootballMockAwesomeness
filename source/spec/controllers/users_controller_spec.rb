568c447c532f624dac40545451be2b22c7e4a58f
fahia mohamed <fahia_mohamed@hotmail.com>
2014-08-08 15:43:01 -0400

    Create test for allowing user to view the index page that contains all draft data.

--- source/spec/controllers/users_controller_spec.rb
+++ source/spec/controllers/users_controller_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
-describe UsersController do
 
-  describe 'GET #new' do
-    it "renders the :new template" do
-      get :new
-      expect(:response).to render_template :new
+describe UsersController do
+  before :each do
+    FactoryGirl.reload
+  end
+  let(:sid) {FactoryGirl.create :user}
+    describe 'GET #new' do
+      it "renders the :new template" do
+        get :new
+        expect(:response).to render_template :new
+      end
     end
   end
+    describe 'create' do
+      it 'should create a user successfully' do
+      post :create, FactoryGirl.attributes_for(:user, user: @user)
+      response.should be_success
+    end
+
+    describe 'index' do 
+      it 'should allow user to view main page' do 
+        get :index 
+        expect(:response).to render_template :index
+      end
+    end
 end
