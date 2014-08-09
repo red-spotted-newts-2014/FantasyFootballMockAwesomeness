373718bdf056fbef4d58281b3243cb361e43ebab
Cassie Moy <moy.cassie@gmail.com>
2014-08-09 16:08:21 -0400

    Add background image to assets.

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -5,12 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# require 'csv'
+
+require 'csv'
 # sid = User.create(email: "sidpatel13@gmail.com", password: "password")
 
-# CSV.foreach('db/fmd_player_rankings.csv', :headers => true, :col_sep => ',') do |row|
-#   Player.create(rank: row[0], first_name: row[3], last_name: row[2], position: row[4], bye_week: row[7], ADP: row[6])
-# end
+CSV.foreach('db/fmd_player_rankings.csv', :headers => true, :col_sep => ',') do |row|
+  Player.create(rank: row[0], first_name: row[3], last_name: row[2], position: row[4], bye_week: row[7], ADP: row[6])
+end
 
 draft = Draft.create(difficulty_level: "easy" , draft_type: "standard" , number_of_rounds: 10, user_id: 1)
 draft2 = Draft.create(difficulty_level: "hard" , draft_type: "standard" , number_of_rounds: 10, user_id: 1)
@@ -21,8 +22,8 @@ draft2 = Draft.create(difficulty_level: "hard" , draft_type: "standard" , number
 end
 
 
-	Player.create(rank: 1, first_name: "john", last_name: "david", position: "RB", bye_week: 7, ADP: 2.27,  pick_id: 1)
-	Player.create(rank: 2, first_name: "harris", last_name: "teeter", position: "RB", bye_week: 7, ADP: 2.27,  pick_id: 2)
-	Player.create(rank: 3, first_name: "wallace", last_name: "lopez", position: "RB", bye_week: 7, ADP: 2.27,  pick_id: 3)
-	Player.create(rank: 4, first_name: "henry", last_name: "davis", position: "RB", bye_week: 7, ADP: 2.27,  pick_id: 4)
-	Player.create(rank: 5, first_name: "jerry", last_name: "garcia", position: "RB", bye_week: 7, ADP: 2.27,  pick_id: 5)
+# Player.create(rank: 1, first_name: "john", last_name: "david", position: "RB", bye_week: 7, ADP: 2.27,  pick_id: 1)
+# Player.create(rank: 2, first_name: "harris", last_name: "teeter", position: "RB", bye_week: 7, ADP: 2.27,  pick_id: 2)
+# Player.create(rank: 3, first_name: "wallace", last_name: "lopez", position: "RB", bye_week: 7, ADP: 2.27,  pick_id: 3)
+# Player.create(rank: 4, first_name: "henry", last_name: "davis", position: "RB", bye_week: 7, ADP: 2.27,  pick_id: 4)
+# Player.create(rank: 5, first_name: "jerry", last_name: "garcia", position: "RB", bye_week: 7, ADP: 2.27,  pick_id: 5)
