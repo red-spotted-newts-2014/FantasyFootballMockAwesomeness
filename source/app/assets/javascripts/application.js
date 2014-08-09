941434599e399a91423da781fff23476298e41b8
Sid Patel <sidpatel13@gmail.com>
2014-08-09 13:51:13 -0400

    cassie_and_anthony s changes

--- source/app/assets/javascripts/application.js
+++ source/app/assets/javascripts/application.js
@@ -1,16 +1,52 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+document.addEventListener('DOMContentLoaded', function(){
+
+  var table = document.querySelector("table");
+  var tbody = document.querySelector("table").querySelector("tbody").children;
+  var rounds = 12;
+  var comp_players = 3;
+
+  // console.log(tbody)
+  for(var pl = 0; pl <= rounds; pl++){
+    for(var num = 0; num < comp_players; num++){
+      var x = table.rows[num+2].insertCell(-1);
+      x.innerHTML="anth"
+    }
+      var x = table.rows[1].insertCell(-1);
+      x.innerHTML="you"
+  }
+
+  var buttons = document.querySelectorAll("button");
+
+  // addEventListener
+  for (var i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", pickPlayer);
+  };
+});
+
+function pickPlayer(e){
+  var playerID = e.target.id;
+  var table = document.querySelector("table");
+
+  addToBoard();
+  removeFromList(e.target);
+  addToPickedBoard(e.target);
+
+}
+
+function addToBoard(){
+    // console.log(table)
+  // var x = table.rows[3].insertCell(-1);
+  // x.innerHTML="New cell";
+};
+
+function addToPickedBoard(e){
+  console.log(e.id);
+  var row = e.parentNode.parentNode;
+  console.log(row.deleteCell(-1));
+  document.querySelector(".picked").appendChild(row);
+};
+
+function removeFromList(e){
+  e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
+};
+
