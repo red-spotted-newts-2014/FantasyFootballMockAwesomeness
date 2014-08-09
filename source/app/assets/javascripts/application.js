c2910fa6ee2f7fc24d05c31f6569aefcc6b7af79
Anthony Edwards Jr & Cassie Moy <pair+aedwards12.cassiemoy@devbootcamp.com>
2014-08-08 20:57:41 -0400

    Work on JS: add event listeners to buttons in "available players" list.

--- source/app/assets/javascripts/application.js
+++ source/app/assets/javascripts/application.js
@@ -1,16 +1,35 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+document.addEventListener('DOMContentLoaded', function(){
+
+  var table = document.querySelector("table");
+  // tbody = document.querySelector("table").querySelector("tbody").children.length
+  var tbody = document.querySelector("table").querySelector("tbody").children;
+  var rounds = 12;
+  var comp_players = 3;
+
+  // console.log(tbody)
+  for(var pl = 0; pl <= rounds; pl++){
+    for(var num = 0; num < comp_players; num++){
+      var x = table.rows[num+2].insertCell(-1);
+      x.innerHTML="anth"
+    }
+      var x = table.rows[1].insertCell(-1);
+      // <user input>
+      x.innerHTML="you"
+  }
+
+  var buttons = document.querySelectorAll("button");
+
+  for (var i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", appendToDraftBoard);
+  };
+
+});
+
+function appendToDraftBoard(e){
+  var playerID = e.target.id;
+  var table = document.querySelector("table");
+  // console.log(table)
+  // var x = table.rows[3].insertCell(-1);
+  // x.innerHTML="New cell";
+}
+
