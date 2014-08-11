2fde143ae4392d50cdae22f69a52c890ce2207fe
Cassie Moy <moy.cassie@gmail.com>
2014-08-10 21:34:37 -0400

    Change border color on tables.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ require 'simplecov'
 
 SimpleCov.start do
   add_filter 'test/'
-  #add_filter 'spec/'
+  # add_filter 'spec/'
   add_filter 'config/'
   add_filter 'vendor/'
   add_filter 'mod_db.rb'
